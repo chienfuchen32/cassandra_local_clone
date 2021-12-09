5d1b6b15006c4968a2e35679b487f3740b591896
陳傑夫 <chienfuchen32@gmail.com>
2021-12-09 20:54:48 +0800

    add user, password, linux user, linux group

--- restore.py
+++ restore.py
@@ -8,6 +8,8 @@ import subprocess
 
 NODE_BACKUP_FOLDER = 'node'
 EXPORT_SCHEMA_FILE = 'schema.cql'
+DEFAULT_CASSANDRA_DATA_PATH = '/var/lib/cassandra/data'
+DEFAULT_BACKUP_PATH = '/cassandra-backup'
 CASSANDRA_GROUP = 'cassandra'
 CASSANDRA_USER = 'cassandra'
 
@@ -22,12 +24,17 @@ class Command():
         return out.decode('utf-8'), err.decode('utf-8')
 
 
-def drop_keyspace(keyspace=''):
+def drop_keyspace(keyspace='', user='', password=''):
     """
     Args:
         keyspace (`string`): specify a keyspace to restore
+        user (`string`): cassandra user
+        password (`string`): cassandra password
     """
-    cmd = ['cqlsh', '-e', 'DROP KEYSPACE IF EXISTS {};'.format(keyspace),
+    cmd = ['cqlsh']
+    if user != '' and password != '':
+        cmd += ['-u', user, '-p', password]
+    cmd += ['-e', 'DROP KEYSPACE IF EXISTS {};'.format(keyspace),
            '--request-timeout=100']
     logging.info(' '.join(cmd))
     out, err = Command.run(cmd)
@@ -36,12 +43,17 @@ def drop_keyspace(keyspace=''):
     logging.info(out)
 
 
-def restore_schema(backup_path=''):
+def restore_schema(backup_path=DEFAULT_BACKUP_PATH, user='', password=''):
     """
     Args:
         backup_path (`string`): Cassandra data path for snapshot
+        user (`string`): cassandra user
+        password (`string`): cassandra password
     """
-    cmd = ['cqlsh', '-f', os.path.join(backup_path, EXPORT_SCHEMA_FILE)]
+    cmd = ['cqlsh']
+    if user != '' and password != '':
+        cmd += ['-u', user, '-p', password]
+    cmd += ['-f', os.path.join(backup_path, EXPORT_SCHEMA_FILE)]
     logging.info(' '.join(cmd))
     out, err = Command.run(cmd)
     logging.error(err)
@@ -49,13 +61,15 @@ def restore_schema(backup_path=''):
     logging.info(out)
 
 
-def chown_data(cassandra_data_path, keyspace):
+def chown_data(cassandra_data_path=DEFAULT_CASSANDRA_DATA_PATH, keyspace='', linux_group=CASSANDRA_GROUP, linux_user=CASSANDRA_USER):
     """
     Args:
         cassandra_data_path (`string`): Cassandra keyspace path to restore
         keyspace (`string`): specify a keyspace to restore
+        linux_group (`string`): cassandra linux group
+        linux_user (`string`): cassandra linux user
     """
-    cmd = ['chown', '-R', '{}:{}'.format(CASSANDRA_USER, CASSANDRA_GROUP),
+    cmd = ['chown', '-R', '{}:{}'.format(linux_group, linux_user),
            os.path.join(cassandra_data_path, keyspace)]
     logging.info(' '.join(cmd))
     out, err = Command.run(cmd)
@@ -64,14 +78,19 @@ def chown_data(cassandra_data_path, keyspace):
     logging.info(out)
 
 
-def refresh_tables(keyspace='', tables=[]):
+def refresh_tables(keyspace='', tables=[], user='', password=''):
     """
     Args:
         keyspace (`string`): specify a keyspace to restore
         tables (list of `string`): tables to refresh
+        user (`string`): cassandra user
+        password (`string`): cassandra password
     """
     for table in tables:
-        cmd = ['nodetool', 'refresh', keyspace, table]
+        cmd = ['nodetool']
+        if user != '' and password != '':
+            cmd += ['-u', user, '-pw', password]
+        cmd += ['refresh', keyspace, table]
         logging.info(' '.join(cmd))
         out, err = Command.run(cmd)
         logging.error(err)
@@ -82,7 +101,11 @@ def refresh_tables(keyspace='', tables=[]):
 def run(backup_path='/cassandra-backup',
         keyspace='',
         snapshot_tag='',
-        cassandra_data_path='/var/lib/cassandra/data'):
+        cassandra_data_path=DEFAULT_CASSANDRA_DATA_PATH,
+        user='',
+        password='',
+        linux_group=CASSANDRA_GROUP,
+        linux_user=CASSANDRA_USER):
     """This is a tool for moving files to restore Cassandra database
     The snapshot folder layout should be:
         cassandra-backup
@@ -109,13 +132,17 @@ def run(backup_path='/cassandra-backup',
         snapshot_tag (`string`): Name for the snapshot directory,
             installation_path/data/keyspace_name/table-UID/snapshots/snapshot_name
         cassandra_data_path (`string`): Cassandra keyspace path to restore
+        user (`string`): cassandra user
+        password (`string`): cassandra password
+        linux_group (`string`): cassandra linux group
+        linux_user (`string`): cassandra linux user
     """
     # reset cassandra keyspace data
-    drop_keyspace(keyspace)
+    drop_keyspace(keyspace, user, password)
     keyspace_path = os.path.join(cassandra_data_path, keyspace)
     if os.path.isdir(keyspace_path):
         shutil.rmtree(os.path.join(cassandra_data_path, keyspace))
-    restore_schema(backup_path)
+    restore_schema(backup_path, user, password)
     snapshot_path = os.path.join(backup_path, snapshot_tag)
     node_backup_path = os.path.join(snapshot_path, NODE_BACKUP_FOLDER)
     list_source = os.listdir(node_backup_path)
@@ -190,10 +217,10 @@ def run(backup_path='/cassandra-backup',
         shutil.rmtree(source_path)
 
     # change file / folder owner to cassandra
-    chown_data(cassandra_data_path, keyspace)
+    chown_data(cassandra_data_path, keyspace, linux_group, linux_user)
 
     # reload from disk
-    refresh_tables(keyspace, list_source)
+    refresh_tables(keyspace, list_source, user, password)
     logging.info('All snapshots has replaced current table db')
 
 
@@ -212,26 +239,37 @@ if __name__ == '__main__':
                    'with tag')
     parser = argparse.ArgumentParser(description=DESCRIPTION,
                                      formatter_class=SmartFormatter)
-    default_cassandra_data_path = '/var/lib/cassandra/data'
     parser.add_argument('--cassandra_data_path', type=str,
                         help=('Cassandra keyspace path to restore, '
                               'defaults to {}').format(
-                              default_cassandra_data_path),
-                        default=default_cassandra_data_path)
+                              DEFAULT_CASSANDRA_DATA_PATH),
+                        default=DEFAULT_CASSANDRA_DATA_PATH)
     parser.add_argument('--keyspace', type=str,
                         help='specify a keyspace to restore',
                         required=True)
     parser.add_argument('--snapshot_tag', type=str,
                         help='Name for the snapshot directory',
                         required=True)
-    default_backup_path = '/cassandra-backup'
     parser.add_argument('--backup_path', type=str,
                         help=('Cassandra data path for snapshot, '
-                              'defaults to {}').format(default_backup_path),
-                        default=default_backup_path)
-
+                              'defaults to {}').format(DEFAULT_BACKUP_PATH),
+                        default=DEFAULT_BACKUP_PATH)
+    parser.add_argument('--user', type=str,
+                        help=('user',
+                        default='')
+    parser.add_argument('--password', type=str,
+                        help=('password',
+                        default='')
+    parser.add_argument('--linux_group', type=str,
+                        help=('cassandra linux group',
+                        default=CASSANDRA_GROUP)
+    parser.add_argument('--linux_user', type=str,
+                        help=('cassandra linux user',
+                        default=CASSANDRA_USER)
     args = parser.parse_args()
 
     # start
     run(args.backup_path, args.keyspace,
-        args.snapshot_tag, args.cassandra_data_path)
+        args.snapshot_tag, args.cassandra_data_path,
+        args.user, args.password,
+        args.linux_group, args.linux_user)
